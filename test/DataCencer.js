3f49da82fdaf0cf5022cc4004640ce80c9a862b1
XatMassacrE <xxat.massacre@gmail.com>
2018-04-23 01:23:34 +0800

    Add test file DataCenter.js

---
+++ test/DataCencer.js
@@ -0,0 +1,45 @@
+var DataCenter = artifacts.require('./DataCenter.sol')
+var web3 = require('web3')
+// return web3.utils.fromAscii(str)
+// return web3.utils.hexToAscii(bytes32)
+
+function getStr(hexStr) {
+  return web3.utils.hexToAscii(hexStr).replace(/\u0000/g, '')
+}
+function getBytes(str) {
+  return web3.utils.fromAscii(str)
+}
+
+contract('DataCenter', accounts => {
+  // account[0] points to the owner on the testRPC setup
+  var owner = accounts[0]
+  var user1 = accounts[1]
+  var user2 = accounts[2]
+  var user3 = accounts[3]
+  var user4 = accounts[4]
+
+  let bet
+  let dataCenter
+  let totalBetAmount = 0
+  let players = []
+  const gameId = '0012345678'
+  const result = '115-109'
+  const hash = 'QmY7kQ3GjZzAW6v622qUe9QoMpJECQa63UkV958kHbuViR'
+  const params = [getBytes(gameId), getBytes(result), getBytes(hash)]
+
+  before(() => {
+    return DataCenter.deployed({from: owner})
+    .then(instance => {
+      dataCenter = instance
+    })
+  })
+
+  it('should return a data item', async () => {
+    await dataCenter.saveResult(...params)
+    const _result = await dataCenter.getResult(getBytes(gameId))
+    const _hash = await dataCenter.getDetailDataHash(getBytes(gameId))
+    console.log(result, hash)
+    assert(getStr(_result), result)
+    assert(getStr(_hash), hash)
+  })
+})
