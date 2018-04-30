4b889bb6b302507c094105689dc102cfb4743701
XatMassacrE <xxat.massacre@gmail.com>
2018-04-30 12:04:27 +0800

    Add confirmResult function
    Maybe need to do some motivation and limitation

--- contracts/DataCenter.sol
+++ contracts/DataCenter.sol
@@ -4,15 +4,31 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract DataCenter is Ownable {
 
+  uint public constant CONFIRM_RESULT_BONUS = 0;
+
   struct DataItem {
     bytes32 gameId;
     bytes32 result;
     string detailDataHash;
+    uint confirmations;
   }
 
-  mapping (bytes32 => DataItem) dataCenter;
+  mapping (bytes32 => DataItem) public dataCenter;
 
-  modifier onlyOnce (bytes32 gameId) {_;}
+  modifier onlyOnce (bytes32 gameId) {
+    require(dataCenter[gameId].gameId != gameId);
+    _;
+  }
+
+  modifier gameExist (bytes32 gameId) {
+    require(dataCenter[gameId].gameId == gameId);
+    _;
+  }
+
+  modifier confirmationNotEnough (bytes32 gameId) {
+    require(dataCenter[gameId].confirmations < 10);
+    _;
+  }
 
   function () public payable {}
   function DataCenter() public {
@@ -23,15 +39,18 @@ contract DataCenter is Ownable {
     dataCenter[gameId].gameId = gameId;
     dataCenter[gameId].result = result;
     dataCenter[gameId].detailDataHash = hash;
+    dataCenter[gameId].confirmations = 0;
   }
 
-  function getResult(bytes32 gameId) view public returns (bytes32) {
-    return dataCenter[gameId].result;
+  function confirmResult(bytes32 gameId, bytes32 result) gameExist(gameId) confirmationNotEnough(gameId) public {
+    if (dataCenter[gameId].result == result) {
+      dataCenter[gameId].confirmations += 1;
+      // rewardERC20(msg.sender);
+    }
   }
 
-  function getDetailDataHash(bytes32 gameId) view public returns (string) {
-    return dataCenter[gameId].detailDataHash;
+  function rewardERC20(address addr) internal {
+    addr.transfer(CONFIRM_RESULT_BONUS);
   }
 }
 
- 
