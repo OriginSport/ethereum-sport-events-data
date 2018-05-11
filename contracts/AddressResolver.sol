c415c6aea14d5c5e52947b4018329649beaa6ca6
XatMassacrE <xxat.massacre@gmail.com>
2018-05-11 15:00:50 +0800

    Add AddressResolver

---
+++ contracts/AddressResolver.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.19;
+
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+contract AddressResolver is Ownable {
+
+  address public addr;
+
+  /**
+   * @dev AddressResolver construct function to
+   *      initial with owner
+   */
+  function AddressResolver() public {}
+
+  /**
+   * @dev get current available address
+   */
+  function getAddress() view public returns (address) {
+    return addr;
+  }
+
+  /**
+   * @dev set a new address(when datacenter contract update)
+   * @param newAddr the new address of datacenter adress
+   */
+  function setAddr(address newaddr) onlyOwner public {
+    addr = newaddr;
+  }
+}
