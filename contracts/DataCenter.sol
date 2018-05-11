dc1ee2ea6a6a48d24f1aa88ab25a2f6356f0daf3
XatMassacrE <xxat.massacre@gmail.com>
2018-05-11 16:05:39 +0800

    Add comments

--- contracts/DataCenter.sol
+++ contracts/DataCenter.sol
@@ -5,7 +5,7 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 contract DataCenter is Ownable {
 
   uint public constant CONFIRM_RESULT_BONUS = 10;
-  uint public constant NEED_CONFIRMATIONS = 12;
+  uint public constant NEED_CONFIRMATIONS = 6;
 
   // NBA all star game pts may greater than 255
   struct DataItem {
@@ -18,16 +18,28 @@ contract DataCenter is Ownable {
 
   mapping (bytes32 => DataItem) public dataCenter;
 
+  /**
+   * @dev save game result only be invoked once
+   * @param gameId indicate the unique id of game
+   */
   modifier onlyOnce (bytes32 gameId) {
     require(dataCenter[gameId].gameId != gameId);
     _;
   }
 
+  /**
+   * @dev make sure game result have saved
+   * @param gameId indicate the unique id of game
+   */
   modifier gameExist (bytes32 gameId) {
     require(dataCenter[gameId].gameId == gameId);
     _;
   }
 
+  /**
+   * @dev prevent user invoke when confirmations is enough
+   * @param gameId indicate the unique id of game
+   */
   modifier confirmationNotEnough (bytes32 gameId) {
     require(dataCenter[gameId].confirmations < NEED_CONFIRMATIONS);
     _;
@@ -38,6 +50,13 @@ contract DataCenter is Ownable {
 
   }
 
+  /**
+   * @dev save game result
+   * @param gameId indicate the unique id of game
+   * @param leftPts the score or points of left team gained(In football left team means home team, in NBA left team means away team)
+   * @param rightPts the score or points of right team gained(In football right team means away team, in NBA left team means home team)
+   * @param hash indicate the IPFS hash of this game s detail data
+   */
   function saveResult(bytes32 gameId, uint16 leftPts, uint16 rightPts, string hash) onlyOwner onlyOnce(gameId) public {
     dataCenter[gameId].gameId = gameId;
     dataCenter[gameId].detailDataHash = hash;
@@ -46,10 +65,20 @@ contract DataCenter is Ownable {
     dataCenter[gameId].confirmations = 1;
   }
 
+  /**
+   * @dev get result of a game and confirmations (simple result)
+   * @param gameId indicate the unique id of game
+   */
   function getResult(bytes32 gameId) view public returns (uint16, uint16, uint8) {
     return (dataCenter[gameId].leftPts, dataCenter[gameId].rightPts, dataCenter[gameId].confirmations);
   }
 
+  /**
+   * @dev encourage user to participant with result audition
+   * @param gameId indicate the unique id of game
+   * @param leftPts the score or points of left team gained(In football left team means home team, in NBA left team means away team)
+   * @param rightPts the score or points of right team gained(In football right team means away team, in NBA left team means home team)
+   */
   function confirmResult(bytes32 gameId, uint leftPts, uint rightPts) gameExist(gameId) confirmationNotEnough(gameId) public {
     if (dataCenter[gameId].leftPts == leftPts && dataCenter[gameId].rightPts == rightPts) {
       dataCenter[gameId].confirmations += 1;
@@ -57,6 +86,10 @@ contract DataCenter is Ownable {
     }
   }
 
+  /**
+   * @dev distribute reward to participants of auditing game result
+   * @param addr the address to receive rewards
+   */
   function rewardERC20(address addr) internal {
     addr.transfer(CONFIRM_RESULT_BONUS);
   }
