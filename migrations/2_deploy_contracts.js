8c9598a3ee1d5e054c2d94222ddeeef9beb92b07
XatMassacrE <xxat.massacre@gmail.com>
2018-05-18 17:31:00 +0800

    Add token address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const DataCenter = artifacts.require('DataCenter.sol')
 
+const testTokenAddr = '0xeb9a4b185816c354db92db09cc3b50be60b901b6'
+const mainTokenAddr = '0x0a22dccf5bd0faa7e748581693e715afefb2f679'
+
 module.exports = function(deployer) {
-  deployer.deploy(DataCenter)
+  deployer.deploy(DataCenter, testTokenAddr)
 }
